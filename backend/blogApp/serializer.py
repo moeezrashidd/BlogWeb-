e15c8cd67a40a177c4f17a13919f8b58b1095ad9
Moeez Rashid <moeezrashidd@gmail.com>
2026-07-11 14:51:11 +0500

    Hash passwords in UserSerializer (make_password) and make password write_only

--- backend/blogApp/serializer.py
+++ backend/blogApp/serializer.py
@@ -1,11 +1,26 @@
 from .models import Users, Posts, Profiles, PostImage
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ['id', 'name', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        if password is not None:
+            validated_data['password'] = make_password(password)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.get('password', None)
+        if password:
+            instance.password = make_password(password)
+            validated_data.pop('password', None)
+        return super().update(instance, validated_data)
 
 
 class PostImageSerializer(serializers.ModelSerializer):
@@ -50,4 +65,3 @@ class ProfilesSerializer(serializers.ModelSerializer):
             'likes',
             'posts',
         ]
-
